cd0f586d846f754c3224850dd1c3154d2d9d9a3e
Shivacki <ishum2005@yandex.com>
2025-06-08 19:18:37 +0300

    feat: change CategoryFetcher to fetch web data

--- src/common/const/fetchPaths.ts
+++ src/common/const/fetchPaths.ts
@@ -1,9 +1,15 @@
 
+export const FETCH_PATHS = {
+  characters: 'https://rickandmortyapi.com/api/character',
+  locations: 'https://rickandmortyapi.com/api/location',
+  episodes: 'https://rickandmortyapi.com/api/episode',
+}
+
+
 const mockPrefix = '/src/mock/';
 
-export const FETCH_PATHS = {
+export const FETCH_PATHS_MOCK = {
   characters: mockPrefix + 'characters.json',
   episodes: mockPrefix + 'episodes.json',
   locations: mockPrefix + 'locations.json',
 }
-
--- src/components/fetch/CategoryFetcher/CategoryFetcher.tsx
+++ src/components/fetch/CategoryFetcher/CategoryFetcher.tsx
@@ -1,23 +1,41 @@
 import { useState, useEffect } from "react";
+import axios from 'axios'
 
+import { CategoryDto } from '@dto/categoryDto'
 import { loadJSON } from '@jsonUtilities'
+
+import styles from './CategoryFetcher.module.css'
 
 
 export interface CategoryFetcherProps<T> {
   fetchPath: string;
   renderData: (data: T | null) => React.ReactNode;
+  isMockData?: boolean;
 }
 
-export default function CategoryFetcher<T>({ fetchPath, renderData }: CategoryFetcherProps<T>) {
+export default function CategoryFetcher<T>({ fetchPath, renderData, isMockData = false }: CategoryFetcherProps<T>) {
 
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState('');
 
-  const fetchData = async () => {
+  const fetchData = async (abortController: AbortController) => {
     setIsLoading(true);
     try {
-      const newData = await loadJSON(fetchPath);
+      let newData;
+      if (isMockData)
+        newData = await loadJSON(fetchPath);
+      else {
+        const response = await axios.get(fetchPath, 
+          {
+            params: {page: 1},
+            signal: abortController.signal,
+          }
+        );
+        // console.log('response.data: ', response.data);
+        newData = (response.data as CategoryDto<T>).results;
+      }
+
       setData(newData as unknown as T);
     } catch(err) {
       setData(null);
@@ -28,7 +46,11 @@ export default function CategoryFetcher<T>({ fetchPath, renderData }: CategoryFe
   }
 
   useEffect(() => {
-    fetchData();
+    const abortController = new AbortController();
+    fetchData(abortController);
+
+    // Прерываем вып-е запроса при размонтировании, если запрос еще не выполнен
+    return () => isLoading ? abortController.abort() : undefined;
   }, []);
   
 
@@ -36,7 +58,7 @@ export default function CategoryFetcher<T>({ fetchPath, renderData }: CategoryFe
     return <>Загрузка...</>
 
   if (!!error)
-    return <>{error}</>
+    return <div className={styles.error}>Ошибка загрузки данных: {error}</div>
 
 
   return  (
